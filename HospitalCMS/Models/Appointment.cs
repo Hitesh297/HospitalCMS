a442c5efa3e840e1a175d3cef36d509c753db1b2
Hitesh <hites_297@yahoo.co.in>
2022-07-01 10:27:16 -0400

    Add project files.

---
+++ HospitalCMS/Models/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HospitalCMS.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentId { get; set; }
+        [ForeignKey("Speciality")]
+        public int SpecialityId { get; set; }
+        public virtual Speciality Speciality { get; set; }
+        [ForeignKey("Speciality")]
+        public int DoctorId { get; set; }
+
+    }
+}
