31f1b398ac6754b6be49874c4d4a967613c23828
Saransh Lakra <saranshlakra4@gmail.com>
2022-07-01 11:04:26 -0400

    Patient modal Created

--- HospitalCMS/Models/Patient.cs
+++ HospitalCMS/Models/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,31 @@ namespace HospitalCMS.Models
 {
     public class Patient
     {
+        [Key]
+        public int PatientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Mobile { get; set; }
+
+        public string Email { get; set; }
+
+        public string MaritalStatus { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string PostalCode { get; set; }
     }
 }
