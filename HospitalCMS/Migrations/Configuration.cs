6317dfb3f4808e3ee4ad99c5a63c813ea43a8792
Kamran Qureshi <kamran_qk@outlook.com>
2022-07-22 22:42:01 -0400

    Add patients, doctors, appointsments and donors

--- HospitalCMS/Migrations/Configuration.cs
+++ HospitalCMS/Migrations/Configuration.cs
@@ -48,16 +48,38 @@ namespace HospitalCMS.Migrations
             context.Doctors.AddOrUpdate(x => x.DoctorId,
                     new Doctor() { DoctorId = 1, Name = "Jane Austen", Email = "jane@gmail.com", Experience = 5, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(1) } },
                     new Doctor() { DoctorId = 2, Name = "Charles Dickens", Email = "charles@gmail.com", Experience = 10, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(2) } },
-                    new Doctor() { DoctorId = 3, Name = "Miguel de Cervantes", Email = "miguel@gmail.com", Experience = 15, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(3) } }
+                    new Doctor() { DoctorId = 3, Name = "Miguel de Cervantes", Email = "miguel@gmail.com", Experience = 15, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(3) } },
+                    new Doctor() { DoctorId = 4, Name = "Shaun Wilson", Email = "shaun@gmail.com", Experience = 7, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(4) } },
+                    new Doctor() { DoctorId = 5, Name = "Fouad Abaza", Email = "fouad@gmail.com", Experience = 10, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(5) } },
+                    new Doctor() { DoctorId = 6, Name = "Corrie Anderson", Email = "corrie@gmail.com", Experience = 12, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(6) } },
+                    new Doctor() { DoctorId = 7, Name = "Mark Trehan", Email = "mark@gmail.com", Experience = 5, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(7) } },
+                    new Doctor() { DoctorId = 8, Name = "Mona Abright", Email = "mona@gmail.com", Experience = 10, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(8) } },
+                    new Doctor() { DoctorId = 9, Name = "Arthur Abbed", Email = "arthur@gmail.com", Experience = 15, Phone = "(123)654-789", Specialities = new List<Speciality>() { context.Specialities.Find(9) } }
                     );
 
             context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 1).FirstOrDefault().Specialities.Add(context.Specialities.Find(18));
             context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 2).FirstOrDefault().Specialities.Add(context.Specialities.Find(19));
             context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 3).FirstOrDefault().Specialities.Add(context.Specialities.Find(20));
+            context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 4).FirstOrDefault().Specialities.Add(context.Specialities.Find(17));
+            context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 5).FirstOrDefault().Specialities.Add(context.Specialities.Find(16));
+            context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 6).FirstOrDefault().Specialities.Add(context.Specialities.Find(15));
+            context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 7).FirstOrDefault().Specialities.Add(context.Specialities.Find(14));
+            context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 8).FirstOrDefault().Specialities.Add(context.Specialities.Find(13));
+            context.Doctors.Include(x => x.Specialities).Where(y => y.DoctorId == 9).FirstOrDefault().Specialities.Add(context.Specialities.Find(12));
 
             context.Patients.AddOrUpdate(x => x.PatientId,
                 new Patient() { PatientId = 1, FirstName = "Roy", LastName = "Isek", Gender = "Male", MaritalStatus = "N/A", Mobile = "123456789", PostalCode = "M9V 3B8", Address1 = "Anabelle Dr.", Address2 = "Willow Street", City = "London", Country = "Canada", DOB = DateTime.Now, Email = "roy@gmail.com" },
-                new Patient() { PatientId = 2, FirstName = "Rids", LastName = "Mave", Gender = "Female", MaritalStatus = "Married", Mobile = "123456789", PostalCode = "M9V 3B8", Address1 = "Arctic Dr.", Address2 = "Orange Street", City = "Toronto", Country = "Canada", DOB = DateTime.Now, Email = "roy@gmail.com" }
+                new Patient() { PatientId = 2, FirstName = "Rids", LastName = "Mave", Gender = "Female", MaritalStatus = "Married", Mobile = "123456789", PostalCode = "M9V 3B8", Address1 = "Arctic Dr.", Address2 = "Orange Street", City = "Toronto", Country = "Canada", DOB = DateTime.Now, Email = "roy@gmail.com" },
+                new Patient() { PatientId = 3, FirstName = "Olivia", LastName = "Abbed", Gender = "Female", MaritalStatus = "N/A", Mobile = "123456789", PostalCode = "M1V 3B8", Address1 = "Wilson Dr.", Address2 = "Wilson Street", City = "Milton", Country = "Canada", DOB = DateTime.Now, Email = "olivia@gmail.com" },
+                new Patient() { PatientId = 4, FirstName = "Emma", LastName = "Lopez", Gender = "Female", MaritalStatus = "Married", Mobile = "123456789", PostalCode = "M2V 3B8", Address1 = "Red Sea Dr.", Address2 = "Red Sea Street", City = "Toronto", Country = "Canada", DOB = DateTime.Now, Email = "emma@gmail.com" },
+                new Patient() { PatientId = 5, FirstName = "Amelia", LastName = "Taylor", Gender = "Female", MaritalStatus = "N/A", Mobile = "123456789", PostalCode = "M3V 3B8", Address1 = "Burk Dr.", Address2 = "Burk Street", City = "Oakvile", Country = "Canada", DOB = DateTime.Now, Email = "amelia@gmail.com" },
+                new Patient() { PatientId = 6, FirstName = "Ava", LastName = "Lee", Gender = "Female", MaritalStatus = "Married", Mobile = "123456789", PostalCode = "M4V 3B8", Address1 = "Trees Dr.", Address2 = "Trees Street", City = "Brampton", Country = "Canada", DOB = DateTime.Now, Email = "ava@gmail.com" },
+                new Patient() { PatientId = 7, FirstName = "Liam", LastName = "Perez", Gender = "Male", MaritalStatus = "N/A", Mobile = "123456789", PostalCode = "M5V 3B8", Address1 = "Rockland Dr.", Address2 = "Rockland Street", City = "London", Country = "Canada", DOB = DateTime.Now, Email = "liam@gmail.com" },
+                new Patient() { PatientId = 8, FirstName = "Noah", LastName = "White", Gender = "Male", MaritalStatus = "Married", Mobile = "123456789", PostalCode = "M7V 3B8", Address1 = "Woodland Dr.", Address2 = "Woodland Street", City = "Berrie", Country = "Canada", DOB = DateTime.Now, Email = "noah@gmail.com" },
+                new Patient() { PatientId = 9, FirstName = "Oliver", LastName = "Clark", Gender = "Male", MaritalStatus = "N/A", Mobile = "123456789", PostalCode = "M9V 3B8", Address1 = "Shore Dr.", Address2 = "Shore Street", City = "London", Country = "Canada", DOB = DateTime.Now, Email = "oliver@gmail.com" },
+                new Patient() { PatientId = 10, FirstName = "James", LastName = "Lewis", Gender = "Male", MaritalStatus = "Married", Mobile = "123456789", PostalCode = "M0V 3B8", Address1 = "Heartland Dr.", Address2 = "Heartland Street", City = "Milton", Country = "Canada", DOB = DateTime.Now, Email = "james@gmail.com" },
+                new Patient() { PatientId = 11, FirstName = "Lucas", LastName = "Walker", Gender = "Male", MaritalStatus = "N/A", Mobile = "123456789", PostalCode = "M6V 3B8", Address1 = "Maxwell Dr.", Address2 = "Maxwell Street", City = "Toronto", Country = "Canada", DOB = DateTime.Now, Email = "lucas@gmail.com" },
+                new Patient() { PatientId = 12, FirstName = "Levi", LastName = "King", Gender = "Male", MaritalStatus = "Married", Mobile = "123456789", PostalCode = "B0V 3B8", Address1 = "Nutty Dr.", Address2 = "Nutty Street", City = "Toronto", Country = "Canada", DOB = DateTime.Now, Email = "levi@gmail.com" }
                 );
 
             context.Departments.AddOrUpdate(x => x.DepartmentId,
@@ -72,10 +94,14 @@ namespace HospitalCMS.Migrations
                 );
 
             context.Appointments.AddOrUpdate(x => x.AppointmentId,
-                new Appointment() { AppointmentId = 1, DoctorId = 1, DoctorNotes = "", PatientId = 1, Schedule = DateTime.Now.AddDays(1)},
+                new Appointment() { AppointmentId = 1, DoctorId = 1, DoctorNotes = "", PatientId = 1, Schedule = DateTime.Now.AddDays(1) },
                 new Appointment() { AppointmentId = 2, DoctorId = 2, DoctorNotes = "", PatientId = 2, Schedule = DateTime.Now.AddDays(2) },
-                new Appointment() { AppointmentId = 3, DoctorId = 3, DoctorNotes = "", PatientId = 1, Schedule = DateTime.Now.AddDays(3) },
-                new Appointment() { AppointmentId = 4, DoctorId = 1, DoctorNotes = "", PatientId = 2, Schedule = DateTime.Now.AddDays(4) }
+                new Appointment() { AppointmentId = 3, DoctorId = 3, DoctorNotes = "", PatientId = 3, Schedule = DateTime.Now.AddDays(3) },
+                new Appointment() { AppointmentId = 4, DoctorId = 4, DoctorNotes = "", PatientId = 4, Schedule = DateTime.Now.AddDays(4) },
+                new Appointment() { AppointmentId = 5, DoctorId = 5, DoctorNotes = "", PatientId = 5, Schedule = DateTime.Now.AddDays(5) },
+                new Appointment() { AppointmentId = 6, DoctorId = 6, DoctorNotes = "", PatientId = 6, Schedule = DateTime.Now.AddDays(6) },
+                new Appointment() { AppointmentId = 7, DoctorId = 7, DoctorNotes = "", PatientId = 7, Schedule = DateTime.Now.AddDays(7) },
+                new Appointment() { AppointmentId = 8, DoctorId = 8, DoctorNotes = "", PatientId = 8, Schedule = DateTime.Now.AddDays(8) }
                 );
 
             context.FAQs.AddOrUpdate(x => x.FAQId,
@@ -91,7 +117,13 @@ namespace HospitalCMS.Migrations
             context.Donors.AddOrUpdate(x => x.DonorId,
                 new Donor() { DonorId = 1, Name = "Jack Ham", DepartmentId = 1, Amount = 2000.00M, Email = "jack@gmail.com", Phone = "(123)123-458" },
                 new Donor() { DonorId = 2, Name = "Max Ruth", DepartmentId = 2, Amount = 3000.00M, Email = "max@gmail.com", Phone = "(123)123-458" },
-                new Donor() { DonorId = 3, Name = "Samantha Obrian", DepartmentId = 3, Amount = 4000.00M, Email = "samantha@gmail.com", Phone = "(123)123-458" }
+                new Donor() { DonorId = 3, Name = "Samantha Obrian", DepartmentId = 3, Amount = 4000.00M, Email = "samantha@gmail.com", Phone = "(123)123-458" },
+                new Donor() { DonorId = 4, Name = "Ella Flores", DepartmentId = 4, Amount = 5000.00M, Email = "Ella@gmail.com", Phone = "(123)123-458" },
+                new Donor() { DonorId = 5, Name = "Avery Green", DepartmentId = 5, Amount = 3000.00M, Email = "Avery@gmail.com", Phone = "(123)123-458" },
+                new Donor() { DonorId = 6, Name = "Sofia Adams", DepartmentId = 6, Amount = 4000.00M, Email = "Sofia@gmail.com", Phone = "(123)123-458" },
+                new Donor() { DonorId = 7, Name = "Luna Carter", DepartmentId = 7, Amount = 1000.00M, Email = "Luna@gmail.com", Phone = "(123)123-458" },
+                new Donor() { DonorId = 8, Name = "Max Evans", DepartmentId = 8, Amount = 7000.00M, Email = "Max@gmail.com", Phone = "(123)123-458" },
+                new Donor() { DonorId = 9, Name = "Jack Diaz", DepartmentId = 1, Amount = 8000.00M, Email = "Jack@gmail.com", Phone = "(123)123-458" }
                 );
 
             context.Events.AddOrUpdate(x => x.EventId,
